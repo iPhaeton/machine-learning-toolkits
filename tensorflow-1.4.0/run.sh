eb0afa20cbadd90304cdffa53933f7c98384bc0e
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-02-06 10:42:40 +0300

    Add pyramda to tensorflow-notebook:1.0.0

--- tensorflow-1.4.0/run.sh
+++ tensorflow-1.4.0/run.sh
@@ -1,3 +1,3 @@
-docker run -d -p 8888:8888 -v "$PWD":/root/work iphaeton/tensorflow-1.4.0:latest;
-sleep 3;
+docker run -d -p 8888:8888 -v "$PWD":/root/work -e DISPLAY=192.168.59.3:0 -v /tmp/.X11-unix:/tmp/.X11-unix:rw iphaeton/tensorflow-1.4.0:latest;
+sleep 1;
 docker logs `docker ps -qf "ancestor=iphaeton/tensorflow-1.4.0:latest"`;
--- tensorflow-jupyter-stack/run.sh
+++ tensorflow-jupyter-stack/run.sh
@@ -1,1 +1,1 @@
-docker run -p 8888:8888 -p 5000:5000 -v "$PWD":/home/jovyan/work iphaeton/tensorflow-notebook:latest
+docker run -p 8888:8888 -p 5000:5000 -v "$PWD":/home/jovyan/work iphaeton/tensorflow-notebook:1.0.0
